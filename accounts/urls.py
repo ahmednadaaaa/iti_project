719740958aae523aec814764ce9acd015cdfbe04
Ahmed Nada <ahmenadaaa@gmail.com>
2025-09-14 16:19:10 +0300

    add the restation

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
+from django.contrib.auth import views as authViews
 
 app_name = 'accounts'
 
@@ -11,4 +12,38 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('profile/delete/', views.delete_account, name='delete_account'),
+
+    path(
+        'reset_password/',
+        authViews.PasswordResetView.as_view(
+            template_name="accounts/password_reset.html",
+            success_url=reverse_lazy('accounts:password_reset_done')
+        ),
+        name="reset_password"
+    ),
+
+    path(
+        'reset_password_sent/',
+        authViews.PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_sent.html"
+        ),
+        name="password_reset_done"
+    ),
+
+    path(
+        'reset/<uidb64>/<token>/',
+        authViews.PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_form.html",
+            success_url=reverse_lazy('accounts:password_reset_complete')
+        ),
+        name="password_reset_confirm"
+    ),
+
+    path(
+        'reset_password_complete/',
+        authViews.PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_done.html"
+        ),
+        name="password_reset_complete"
+    ),
 ]
--- crowdfunding/urls.py
+++ crowdfunding/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 from .api import *
-
 app_name = 'crowdfunding'
 
 urlpatterns = [
